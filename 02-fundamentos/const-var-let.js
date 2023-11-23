53bc504dc0b20f2bfc4ee4671b6ffc533e4a56f1
IngridFLG <ingrid.florez0226@gmail.com>
2023-11-23 16:31:07 -0500

    constantes y strings

---
+++ 02-fundamentos/const-var-let.js
@@ -0,0 +1,14 @@
+
+const nombre = 'Jeff';
+
+
+
+if (true) {
+    const nombre = 'Hola';
+
+    console.log(nombre);
+}
+
+
+
+console.log(nombre);
---
+++ 02-fundamentos/template-string.js
@@ -0,0 +1,17 @@
+const nombre = 'Hola';
+const saludo = 'como esta';
+
+
+const saludoCompleto = nombre + ' ' + saludo;
+const template = `${nombre} ${saludo}`;
+
+console.log(saludoCompleto);
+console.log(template);
+
+
+
+const html  = `
+<h1>Hola</h1>
+<p>Mundo</p>`;
+
+console.log(html);
