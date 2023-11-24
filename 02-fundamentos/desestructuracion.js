5c59a0a7aba84c865e2159471758014b8e4db9ce
IngridFLG <ingrid.florez0226@gmail.com>
2023-11-24 15:31:38 -0500

    desestructuracion de objetos

---
+++ 02-fundamentos/desestructuracion.js
@@ -0,0 +1,40 @@
+
+
+
+
+const deadpool = {
+    nombre: 'Wade',
+    apellido: 'Winston',
+    poder: 'Regeneración',
+    edad: 50,
+    getNombre(){
+        return `${ this.nombre } ${ this.apellido } ${this.poder }`
+    }
+}
+
+
+// const nombre  = deadpool.nombre;
+// const apellido = deadpool.apellido;
+// const poder = deadpool.poder;
+
+
+function imprimeHeroe( { nombre, apellido, poder, edad } ) {
+    // const { nombre, apellido, poder, edad } = heroe;
+    console.log(nombre, apellido, poder, edad);
+}
+
+
+imprimeHeroe(deadpool);
+
+// const { nombre, apellido, poder, edad = 0 } = deadpool;
+// console.log(nombre, apellido, poder, edad);
+
+const heroes = [ 'Deadpool', 'Superman', 'Batman'];
+
+// const h1 = heroes[0];
+// const h2 = heroes[1];
+// const h3 = heroes[2];
+
+const [ , , h3 ] = heroes;
+
+console.log(h3);
